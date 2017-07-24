20b979895c319fd7655d7b7c84c0da3c9d2c5e33
arjunkashyap <arjun.kashyap@yahoo.co.in>
2017-07-24 13:48:10 +0530

    electron initialized

--- app.js
+++ app.js
@@ -1,46 +1,48 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
+module.exports = () => {
+	const express = require('express');
+	const path = require('path');
+	const bodyParser = require('body-parser');
 
-// Init app
-const app = express();
+	// Init app
+	const app = express();
 
-// Bring in Models
-let Article = require('./models/article');
+	// Bring in Models
+	let Article = require('./models/article');
 
-// Load View engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+	// Load View engine
+	app.set('views', path.join(__dirname, 'views'));
+	app.set('view engine', 'pug');
 
-// Body parser Middleware
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
-app.use(bodyParser.json());
-
-
-// Set Public folder
-app.use(express.static(path.join(__dirname, 'public')));
+	// Body parser Middleware
+	// parse application/x-www-form-urlencoded
+	app.use(bodyParser.urlencoded({ extended: false }));
+	// parse application/json
+	app.use(bodyParser.json());
 
 
-// Home Route
-app.get('/', function(req, res){
+	// Set Public folder
+	app.use(express.static(path.join(__dirname, 'public')));
 
-	res.render('index', {
 
-		title: 'Bhavan\'s Journal'
+	// Home Route
+	app.get('/', function(req, res){
+
+		res.render('index', {
+
+			title: 'Bhavan\'s Journal'
+		});
 	});
-});
 
-// Route Files
-let articles = require('./routes/articles');
-app.use('/articles', articles);
+	// Route Files
+	let articles = require('./routes/articles');
+	app.use('/articles', articles);
 
-let article = require('./routes/article');
-app.use('/article', article);
+	let article = require('./routes/article');
+	app.use('/article', article);
 
-// Start Server
-app.listen(3000, function(){
+	// Start Server
+	app.listen(3000, function(){
 
-	console.log('Server started on port 3000');
-});
+		console.log('Server started on port 3000');
+	});
+}
---
+++ main.js
@@ -0,0 +1,44 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win;
+
+function createWindow () {
+
+	// Instantiate Express App
+	app.server = require(__dirname + '/app')();
+
+	// Create the browser window.
+	win = new BrowserWindow();
+	// win.maximize();
+
+	// and load the index.html of the app.
+	win.loadURL('http://localhost:3000');
+
+
+	// Open the DevTools.
+	// win.webContents.openDevTools();
+
+	win.focus();
+
+	// Emitted when the window is closed.
+	win.on('closed', () => {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		win = null
+	});
+}
+
+// Run create window function
+app.on('ready', createWindow);
+
+// Quit when allwindows are closed
+app.on('window-all-closed', function(){
+
+	if(process.platform !== 'darwin'){
+
+		app.quit();
+	}
+});
