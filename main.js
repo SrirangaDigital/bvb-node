66d578d60b242535e4eb055462b49e7cd6d8b0c1
arjunkashyap <arjun.kashyap@yahoo.co.in>
2017-07-27 18:10:52 +0530

    pdf now served through electron-pdf-window

--- main.js
+++ main.js
@@ -11,12 +11,21 @@ function createWindow () {
 
 	// Create the browser window.
 	win = new BrowserWindow({
+	
+		show: false,
 		icon: path.join(__dirname, '/resources/app/public/img/logo.png')
 	});
+	
 	win.maximize();
 
 	// and load the index.html of the app.
-	win.loadURL('http://localhost:3000');
+	win.loadURL('http://localhost:3000/');
+	// win.loadURL(`file://${__dirname}/index.html`);
+	
+	// win.webContents.on('did-finish-load', function() {
+	
+	// 	win.show();
+	// });
 
 
 	// Open the DevTools.
--- resources/app/public/js/main.js
+++ resources/app/public/js/main.js
@@ -1,5 +1,23 @@
+window.jQuery = window.$ = require('jquery');
+const electron = require('electron')
+const PDFWindow = require('electron-pdf-window')
+const { BrowserWindow } = electron.remote
+
 $(document).ready(function() {
 
-	console.log('Hello');
+	$('.pdf').on('click', function(event){
+
+		event.preventDefault();
+		var pdfURL = $(this).attr('href');
+
+		const win = new BrowserWindow({
+	
+			width: 800,
+			height: 600
+		})
+		PDFWindow.addSupport(win)
+
+		win.loadURL('http://localhost:3000/' + pdfURL);
+	});
 
 });
